39a7e8a1c8913fc81f690d1579011be132d7df75
Alexander <ezharjan@aliyun.com>
2020-06-13 21:51:05 +0800

    the way of building angular projects

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-demo';
+  count = 500;
+
+  increament = function () {
+    console.log(this.count++);
+  }
+}
